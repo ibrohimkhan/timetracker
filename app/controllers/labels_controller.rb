679505e1e480e7524d0e9cf7e003aa819c7c6d92
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-08-04 21:02:57 +0500

    Update labels_controller.rb

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -2,41 +2,41 @@ class LabelsController < ApplicationController
 
   before_filter :find_label, :only => [:show, :edit, :update, :destroy]
 
-	def index
-		@labels = Label.all
-	end
+  def index
+    @labels = Label.all
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@label = Label.new
-	end
+  def new
+    @label = Label.new
+  end
 
-	def create
+  def create
     @label = Label.new(params.require(:label).permit(:name))
-		if @label.save
-			redirect_to labels_path
-		else
-			render :new
-		end
-	end
+    if @label.save
+      redirect_to labels_path
+    else
+      render :new
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
+  def update
     if @label.update(params.require(:label).permit(:name))
       redirect_to labels_path
     else
       render :edit
     end
-	end
+  end
 
-	def destroy
-		@label.destroy
-		redirect_to labels_path
-	end
+  def destroy
+    @label.destroy
+    redirect_to labels_path
+  end
 
   private
   def find_label
