74761230a709a741b1b3c507966d9cb13f26bc2e
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-08-05 14:30:08 +0000

    removed labels in route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-	  def configure_permitted_parameters
-	    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, 
-	    	:password, :password_confirmation) }
-	  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   resources :tasks
-  resources :labels
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
