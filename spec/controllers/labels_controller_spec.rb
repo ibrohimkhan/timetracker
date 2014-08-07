8856396044aecc92fe2152cd9d1e05e92ae7a8d1
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2014-08-07 13:16:20 +0000

    started testing json format. In progress

--- spec/controllers/labels_controller_spec.rb
+++ spec/controllers/labels_controller_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe LabelsController, :type => :controller do
+describe LabelsController do
+
+  context "when user logged in" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:label) { Factory.create(:label) }
+    subject { label }
+
+    before do
+      sign_in user
+    end
+
+    describe "POST #create" do
+      context "with valid attributes" do
+        it "creates new object" do
+          #json = { :format => 'json', :application => { label: { :name => "foo" } } }
+          #post :create, json
+          #expect{post :create, {label: {name: 'hi'}}}.to change(Label, :count).by(1)
+        end
+      end
+    end
+  end
 
 end
